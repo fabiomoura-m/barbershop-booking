25dc4ef7a0eda4a2ed243f152df5d310cea61553
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-02-01 15:19:11 -0300

    feat: add initial search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,20 +1,25 @@
 import { format } from 'date-fns';
 import Header from '../_components/header';
 import { ptBR } from 'date-fns/locale';
+import Search from './_components/search';
 
 export default function Home() {
     return (
         <div>
             <Header />
 
-          <div className='px-5 py-6'>
-            <h2 className='text-xl font-bold'>Olá, Fabio!</h2>
-            <p className="capitalize">
-                {format(new Date(), "EEEE',' d 'de' MMMM", { locale: ptBR })}
-            </p>
-          </div>
+            <div className="px-5 pt-6">
+                <h2 className="text-xl font-bold">Olá, Fabio!</h2>
+                <p className="capitalize">
+                    {format(new Date(), "EEEE',' d 'de' MMMM", {
+                        locale: ptBR
+                    })}
+                </p>
+            </div>
 
-          
+            <div className="px-5 mt-6">
+                <Search />
+            </div>
         </div>
     );
 }
