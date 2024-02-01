771700c7847e754fbffed33f93e7fe3be65e42aa
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-02-01 15:04:44 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,20 @@
-import Header from "../_components/header";
+import { format } from 'date-fns';
+import Header from '../_components/header';
+import { ptBR } from 'date-fns/locale';
 
 export default function Home() {
-  return (
-    
-    <div>
-      <Header/>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+
+          <div className='px-5 py-6'>
+            <h2 className='text-xl font-bold'>Olá, Fabio!</h2>
+            <p className="capitalize">
+                {format(new Date(), "EEEE',' d 'de' MMMM", { locale: ptBR })}
+            </p>
+          </div>
+
+          
+        </div>
+    );
 }
