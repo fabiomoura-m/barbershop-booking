7b80edeeb361f9b2c5c17a3210675453360cd03a
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-02-21 15:34:26 -0300

    fix: ensure auth options are exported from _lib folder

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -3,7 +3,7 @@ import BarbershopInfo from './_componentes/barbershop-info';
 import ServiceItem from './_componentes/service-item';
 import { Barbershop, Service } from '@prisma/client';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { authOptions } from '@/app/_lib/auth';
 
 interface BarbershopDetailsPageProps {
     params: {
